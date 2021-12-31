fa300dae4ba288cc0968c787a954715f9d5b98dd
AugustoDuart3 <84602076+AugustoDuart3@users.noreply.github.com>
2021-12-31 17:30:44 -0300

    Update 211231OrderConcepts.vb

--- 211231OrderConcepts.vb
+++ 211231OrderConcepts.vb
@@ -1,23 +1,22 @@
 'Some VBA code made to order a long accounting sheet
 
-Sub acomodar_conceptos()
-'comienza en a3 la lista, termina en a8088(8087 ultima llena). eso lo mido con columna b vacia(condicion del loop)
+Sub order_concepts()
+'starts at a3
 Range("a3").Select
 
-Dim separador As String
-separador = " // "
+Dim separator As String
+separator = " // "
 
 Do While ActiveCell.Offset(0, 0).Range("b1").Value <> ""
     Do While ActiveCell.Value <> "" And ActiveCell.Offset(0, 0).Range("a2").Value = ""
-        'NO cambiar foco de offset aca
-        'corta lo de a2 y lo une a lo de a1 separado por /
-        ActiveCell.Offset(0, 0).Range("b1").Value = ActiveCell.Offset(0, 0).Range("b1").Value & separador & ActiveCell.Offset(0, 0).Range("b2").Value
-        'elimina la fila de a2
+        'DO NOT change offsets focus here
+        'cuts whats below and unites that with whats above, with the separator
+        ActiveCell.Offset(0, 0).Range("b1").Value = ActiveCell.Offset(0, 0).Range("b1").Value & separator & ActiveCell.Offset(0, 0).Range("b2").Value
+        'deletes the row below
         ActiveCell.Offset(0, 0).Range("a2").EntireRow.Delete
-        'vuelve a activar el primer active cell para que se repita el loop de ser necesario(quiza esta de mas)
+        'activates once again the first active cell so that the loop repeats if necessary
     Loop
-ActiveCell.Offset(1, 0).Range("a1").Select 'bajo una y repito
+ActiveCell.Offset(1, 0).Range("a1").Select 'goes one row below, repeats
 
 Loop
-'chequear si hay una ultima fecha mal(todas deberian quedar seguidas)
 End Sub
