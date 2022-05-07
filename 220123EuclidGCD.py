aff3a98a453519788cb2aff97ca229905a8a522a
Augusto Duarte <84602076+AugustoDuart3@users.noreply.github.com>
2022-05-07 19:05:39 -0300

    Update 220123EuclidGCD.py

--- 220123EuclidGCD.py
+++ 220123EuclidGCD.py
@@ -4,8 +4,24 @@
 def GreatestCommonDivisorRecursive(a,b):
     #base
     if(b==0): return a
-    if(a==0): return b
-    if(a==b): return b
+    if(a==0 or a==b): return b
     #recursive, use the place of the biggest one for the remainder
     if(b>a): return GreatestCommonDivisorRecursive(a,b%a)
     if(a>b): return GreatestCommonDivisorRecursive(a%b,b)
+
+    
+def GreatestCommonDivisor(a,b):
+    #unfinised
+    #edge cases: both zeroes, only one 0
+    if(a==b): return b
+    if(a<b):
+        GreaterValue=b
+        LesserValue=a
+    if(a>b):
+        GreaterValue=a
+        LesserValue=b
+    while(LesserValue>0):
+        Stored=LesserValue #store it for a line
+        LesserValue=GreaterValue%LesserValue
+        GreaterValue=Stored
+    return GreaterValue
